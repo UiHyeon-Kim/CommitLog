0dc4948e4ed0aa44488634a32e8da78789f515c1
김의현 <hyunkim6790@gmail.com>
2026-03-26 12:57:21 +0900

    refactor: 리컴포지션 최적화를 위해 오프셋 람다 버전 적용

--- presentation/src/main/java/com/hanhyo/commitlog/presentation/ui/stats/components/StatsPeriodTabs.kt
+++ presentation/src/main/java/com/hanhyo/commitlog/presentation/ui/stats/components/StatsPeriodTabs.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import com.hanhyo.commitlog.presentation.R
@@ -60,7 +61,7 @@ fun StatsPeriodTabs(
         // 슬라이딩 인디케이터 배경
         Box(
             modifier = Modifier
-                .offset(x = indicatorOffset)
+                .offset { IntOffset(x = indicatorOffset.roundToPx(), y = 0) }
                 .width(tabWidth)
                 .fillMaxHeight()
                 .background(color = CommitLogTheme.colors.primary, shape = CircleShape)
